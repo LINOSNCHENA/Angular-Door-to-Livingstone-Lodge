fc7cb27dfb4d91fd36a02d3f637a0c29b369710f
linos nchena <linosnchena@gmail.com>
2020-10-25 22:30:07 +0100

    rearrange the images

--- FrontEnd/src/app/app.component.spec.ts
+++ FrontEnd/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'clientx'`, () => {
+  it(`should have as title 'Livingstone-Safaries'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('clientx');
+    expect(app.title).toEqual('Livingstone-Safaries');
   });
 
   // it('should render title', () => {
   //   const fixture = TestBed.createComponent(AppComponent);
   //   fixture.detectChanges();
   //   const compiled = fixture.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain('clientx app is running!');
+  //   expect(compiled.querySelector('.content span').textContent).toContain('Livingstone-Safaries app is running!');
   // });
 });
