1b4bcc1b69364c80b62382df2ef3e2c5d0722d84
LINOS NCHENA <linosnchena@gmail.com>
2020-10-27 16:41:13 +0100

    added insert data

--- schemaMySQL.sql
+++ schemaMySQL.sql
@@ -4,20 +4,20 @@ CREATE TABLE users
   id serial PRIMARY KEY,
   email VARCHAR(90) NOT NULL,
   password VARCHAR(650) NOT NULL,
-  mobile VARCHAR(90) default 'CONTRATOR',
-  office VARCHAR(90) default 'CONTRATOR',
-  createdAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updatedAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  mobile VARCHAR(90) default 'M-994011004n',
+  office VARCHAR(90) default 'O-891000003n',
+  createdat TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updatedat TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   UNIQUE(email,password)
 );
 
 INSERT INTO users
   (id,email, password)
 VALUES
-  (1, 'nikolasmarvin@yahoo.com', '+230775263158'),
-  (2, 'Lorenamarvin@yahoo.com', '+230775263158'),
-  (3, 'LeonCathy@yahoo.com', '+230775263158'),
-  (4, 'cathyNoria@yahoo.com', '+230775263158');
+  (1, 'nikolas.marvin@hotmail.com', '+230775263158'),
+  (2, 'Lorena.linos@yahoo.com', '+230775263158'),
+  (3, 'Leon.catherine@gmail.com', '+230775263158'),
+  (4, 'cathy.noria@yahoo.com', '+230775263158');
 
 SELECT *
 from users;
